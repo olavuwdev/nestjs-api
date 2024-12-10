082599abc9f7b4c99067de90c23ade6c9aae6020
Olavo Adriel <ollavoadriel@gmail.com>
2024-12-10 20:37:35 -0300

    Criando conteiner

---
+++ src/routes/routes.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CreateRouteDto } from './dto/create-route.dto';
+import { UpdateRouteDto } from './dto/update-route.dto';
+import { PrismaService } from './../prisma/prisma.service';
+
+@Injectable()
+export class RoutesService {
+
+  constructor(private PrismaService: PrismaService){}
+
+  create(createRouteDto: CreateRouteDto) {
+    return 'This action adds a new route';
+  }
+
+  findAll() {
+    return `This action returns all routes`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} route`;
+  }
+
+  update(id: number, updateRouteDto: UpdateRouteDto) {
+    return `This action updates a #${id} route`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} route`;
+  }
+}
