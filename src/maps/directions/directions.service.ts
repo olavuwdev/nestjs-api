631c8d4bc05b26f321f6da4bea13d3701a085040
Olavo Adriel <ollavoadriel@gmail.com>
2024-12-10 17:56:26 -0300

    Ajuste na api

--- src/maps/directions/directions.service.ts
+++ src/maps/directions/directions.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Client as GoogleMapsClient } from '@googlemaps/google-maps-services-js';
+import { DirectionsRequest, Client as GoogleMapsClient, TravelMode } from '@googlemaps/google-maps-services-js';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
@@ -8,8 +8,42 @@ export class DirectionsService {
         private googleMapsClient: GoogleMapsClient,
         private configService: ConfigService
     ) {}
-    getDirections(originId: string, destinationId:string){
+    async getDirections(originId: string, destinationId:string){
+
+        const requestParams: DirectionsRequest['params'] ={
+            origin: `place_id:${originId}`,
+            destination:`place_id:${destinationId}`,
+            mode:TravelMode.driving,
+            key:this.configService.get('GOOGLE_MAPS_API_KEY'),
+        }
+
+        const { data } = await this.googleMapsClient.directions({
+            params: requestParams,
+        })
+
+        return { 
+            ...data,
+            request:{
+                origin:{
+                    place_id: requestParams.origin,
+                    location: {
+                        lat:data.routes[0].legs[0].start_location.lat,
+                        lng:data.routes[0].legs[0].start_location.lng
+                    },
+                },
+                destination:{
+                    origin:{
+                        place_id: requestParams.destination,
+                        location: {
+                            lat:data.routes[0].legs[0].end_location.lat,
+                            lng:data.routes[0].legs[0].end_location.lng
+                        },
+                            },
+                mode:requestParams.mode
+            },
+        }
         
     }
+}
 
 }
